fe69554416badf58ffda396e0d2410f69e716af0
rockwang <rockwang@localhost.localdomain>
2019-02-01 18:42:08 +0800

    HMR: using webpack-dev-middleware ,not the way of using webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -2,19 +2,35 @@ import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
 import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
     let element = document.createElement('div');
+    var btn = document.createElement('button');
 
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
     element.classList.add('hello');
     var myIcon = new Image();
     myIcon.src = Icon;
     element.appendChild(myIcon);
+    element.appendChild(btn);
 
     console.log(Data);
     return element;
 }
 
-document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
+
+if (module.hot) {
+    module.hot.accept('./print.js', function() {
+        console.log('Accepting the updated printMe module!');
+        document.body.removeChild(element);
+        element = component(); // Re-render the "component" to update the click handler
+        document.body.appendChild(element);
+    })
+  }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        // https://github.com/webpack-contrib/webpack-hot-middleware
+        // point 2:
+        app: ['./src/index.js', 'webpack-hot-middleware/client'],
+        print: ['./src/print.js', 'webpack-hot-middleware/client']
+
+        // or:
+// https://github.com/webpack-contrib/webpack-hot-middleware/blob/master/example/webpack.config.js
+    },
+    devtool: 'inline-source-map',
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     },
     module: {
         rules: [
@@ -41,5 +54,14 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            title: 'Output Management'
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
 };
